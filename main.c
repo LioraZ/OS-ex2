b58078dff31f611c23ce4afc4d715956c675ab1b
LioraZ <liorazr@gmail.com>
2018-04-10 14:36:03 +0300

    First commit

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+typedef struct {
+
+
+} Job;
+typedef struct {
+    (void *)void push(Job *job);
+    (void *)Job* pop();
+} JobsQueue;
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}

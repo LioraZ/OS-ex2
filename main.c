4e3a61ca41921b99017711ff6ab12fc0c16e7196
LioraZ <liorazr@gmail.com>
2018-04-10 15:25:49 +0300

    started implementing jobs queue

--- main.c
+++ main.c
@@ -1,15 +1,62 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef struct {
-
-
+    int pid;
+    struct Job *next;
 } Job;
+
 typedef struct {
-    (void *)void push(Job *job);
-    (void *)Job* pop();
+    Job *first;
+    Job *last;
+    Job *secondToLast;
+    int size;
 } JobsQueue;
 
+Job *newJob(int n_pid) {
+    Job *job = (Job *)malloc(sizeof(Job));
+    if ()
+    job->pid = n_pid;
+    job->next = NULL;
+}
+JobsQueue *createJobQueue(Job *job) {
+    if (!job) return NULL;
+    JobsQueue *jobsQueue = (JobsQueue*)malloc(sizeof(JobsQueue));
+    if (!jobsQueue) return NULL;
+    jobsQueue->first = job;
+    jobsQueue->secondToLast = job;
+    jobsQueue->last = job;
+    jobsQueue->size = 1;
+}
+void addJob(JobsQueue *jobsQueue, Job *job) {
+    if (!job || !jobsQueue) return;
+    jobsQueue->secondToLast = jobsQueue->last;
+    jobsQueue->last = job;
+    (jobsQueue->size)++;
+}
+void removeJob(JobsQueue *jobsQueue) {
+    Job *temp = jobsQueue->first;
+    jobsQueue->first = (Job *)jobsQueue->first->next;
+    free(temp);
+    (jobsQueue->size)--;
+}
+void freeJobsQueue(JobsQueue *jobsQueue) {
+    Job *curr = jobsQueue->first;
+    while (curr != NULL) {
+        Job *temp = curr;
+        curr = curr->next;
+        free(temp);
+    }
+}
+int isEmpty(JobsQueue *jobsQueue) { return jobsQueue->size == 0; }
+
+char* getPromptJob();
+
 int main() {
-    printf("Hello, World!\n");
-    return 0;
+    JobsQueue *jobsQueue = (JobsQueue*)malloc(sizeof(JobsQueue));
+    char *promptJob = getPromptJob();
+    Job *job = (Job *)job.newJob()
+    while (1) {
+
+    }
 }
